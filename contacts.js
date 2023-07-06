babbccb8223527d79989cada4a1d2344d5d9ea4f
Volodymyr <mv.design8@gmail.com>
2023-07-06 15:14:01 +0300

    fix

--- contacts.js
+++ contacts.js
@@ -1,4 +1,3 @@
-
 const { nanoid } = require("nanoid");
 const path = require("path");
 const fs = require("fs").promises;
@@ -12,22 +11,33 @@ async function listContacts() {
 
 async function getContactById(contactId) {
   const data = JSON.parse(await fs.readFile(contactsPath, "utf8"));
-  const contact = data.find((contact) => contact.id === contactId);
-  console.table(contact);
+  const contactById = data.find((contact) => contact.id === contactId);
+  console.log(contactById ? contactById : `${null} - Такого контакту не існує`);
 }
 
 async function removeContact(contactId) {
   const data = JSON.parse(await fs.readFile(contactsPath, "utf8"));
   const contact = data.filter((contact) => contact.id !== contactId);
+  await getContactById(contactId);
   await fs.writeFile(contactsPath, JSON.stringify(contact), "utf8");
-  console.table(contact);
 }
 
 async function addContact(name, email, phone) {
   const data = JSON.parse(await fs.readFile(contactsPath, "utf8"));
-  data.push({ name, email, phone, id: nanoid(7) });
+  const contactId = nanoid(7);
+  if (
+    data.find(
+      (contact) =>
+        contact.name === name ||
+        contact.email === email ||
+        contact.phone === phone
+    )
+  )
+    return console.log("Такий контакт вже існує");
+  data.push({ name, email, phone, id: contactId });
   await fs.writeFile(contactsPath, JSON.stringify(data), "utf8");
-  console.table(data);
+  await getContactById(contactId);
+  // console.table(newContact);
 }
 
 module.exports = {
